086cf3189c3adb796e8c73ad952879cc60787a66
VivekNair2 <vivekh2004@gmail.com>
2024-12-01 12:43:30 +0530

    first commit

---
+++ tests/test_operations.py
@@ -0,0 +1,10 @@
+from src.mathoperation import add , subtract
+
+def test_add():
+    assert add(2,3)==5
+    assert add(-2,3)==1
+    
+    
+def subtract():
+    assert subtract(2,3)==5
+    assert subtract(-2,3)==-5
