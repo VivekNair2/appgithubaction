0a385f10bf682ea4f2705f51a29e86a616a86285
VivekNair2 <vivekh2004@gmail.com>
2024-12-01 13:32:01 +0530

    third commit

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,13 +1,15 @@
-import src
-from src.mathoperation import add,subtract
+import sys
+import os
 
+# Add the parent directory to the Python path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from src.mathoperation import add, subtract
 
 def test_add():
-    assert add(2,3)==5
-    assert add(-2,3)==1
-    
-    
-def subtract():
-    assert subtract(2,3)==5
-    assert subtract(-2,3)==-5
-    
+    assert add(2, 3) == 5
+    assert add(-2, 3) == 1
+
+def test_subtract():
+    assert subtract(5, 3) == 2
+    assert subtract(-2, 3) == -5
